f8ea8bdf1983032e07181734594b8f4c74318a66
Billy bob <d.jorgensen@imaginelearning.com>
2012-07-11 10:43:30 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lms::Application.config.secret_token = 'a5ad1e50ebf2ebf5de9ad657950347744addf0d4347f8edb4d33fddb7a11205cfa345c2d2c5cefbcb11158095a19853a688eaf9ede8d0ac265307ba74fd80786'
